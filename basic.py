10cd082bd587cf7f1d33f69d382ce554a9bbeeac
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-23 06:03:24 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,36 @@
+# displaying something on the terminal 
+# print("Hello, Megan")
+
+# it takes input from the user 
+# name = input("Enter your name:")
+
+# we use # symbol to make a comment 
+
+# making a copy of the github folder in your computer 
+# git clone github_link 
+
+# shortcuts 
+# arrow keys are used to get the old command back 
+# code . 
+# ctrl / = comment without writing 
+
+# to run the python file in terminal 
+# py basic.py  
+
+# print('megan')
+# print('''megan''')
+# print("""megan""")
+
+# this doesn't work 
+# print(''''megan'''')
+
+# we can use single quotes, double quotes, triple single quotes,
+# triple double quotes in the print statement in python 
+
+# multiline comment 
+'''
+hi my name is megan
+i am studying in first grade 
+'''
+
+# spaces are so critical in python, you cannot put any extra space 
