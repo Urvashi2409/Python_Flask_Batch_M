397bd1d573cfedb6bacffb25c2a7457028784e94
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-27 21:33:14 +0530

    type casting, case sensitivity

--- basic.py
+++ basic.py
@@ -6,12 +6,14 @@
 
 # we use # symbol to make a comment 
 
+# comments = that part of code which is not executed by python and pythonn ignores it 
+ 
 # making a copy of the github folder in your computer 
 # git clone github_link 
 
 # shortcuts 
 # arrow keys are used to get the old command back 
-# code . 
+# code . = to open the vscode 
 # ctrl / = comment without writing 
 
 # to run the python file in terminal 
@@ -34,3 +36,201 @@ i am studying in first grade
 '''
 
 # spaces are so critical in python, you cannot put any extra space 
+
+# data types in python 
+
+# integer  = all the numbers from negative to positive side 
+# float = all the  numbers with decimal points 
+# double =  very big decimal point number  
+# string = group of characters(alphabets, numbers, symbols, spaces )
+# bool = True/False 
+# char = 'a' '0', '-', ':', '(' = single characters are stored in char  
+
+# variable = it is a container to store something in the memory 
+
+# integers
+# num = 45 
+# print(num)
+# print(type(num))
+
+# float 
+# num = 5.60
+# print(num)
+# print(type(num))
+
+# double 
+# num = 55890.74454523485233283223432432854324538434532453242345238453254324832
+# print(num)
+
+# char 
+# mychar = '6'
+# print(mychar)
+
+# string 
+# mystr = "Megan and Urvashi"
+# print(mystr)
+# print(type(mystr))
+
+# bool 
+# variable = True 
+# print(variable)
+# print(type(variable))
+
+# ************** variable naming conventions ********** (how you can make a variable)
+# 1.you can write any alphabet including lowercase and uppercase in the variable 
+# example: num, number, string, megan, urvashi, URVASHI 
+# 2. Numbers can be added in a variable but not at the first place 
+# example: megan45, go4578tu, 
+# 45tyu --> this is wrong 
+# 3. Symbols are not allowed in the variable except _( underscore )
+# example: num_, _num, n_um 
+# 4. spaces are not allowed in the variable 
+# example: nu m --> this is wrong 
+
+# m = "Megan"
+# print(m)
+
+# M = "Megan"
+# print(M)
+
+# Number = 34 
+# print(Number)
+
+# m23 = "Megan"
+# print(m23)
+
+# m45M = "Megan"
+# print(m45M)
+
+# 45m = "Megan"
+# print(45m)
+
+# number% = 34 
+# print(number%)
+
+# number^ = 56 
+# print(number^)
+
+# num^ber = 4 
+# print(num^ber)
+
+# ^number = 6 
+# print(^number)
+
+# number$ = 5 
+# print(number$)
+
+# number_ = 23 
+# print(number_)
+
+# _number = 4 
+# print(_number)
+
+# num_ber = 5 
+# print(num_ber)
+
+# num ber = 45 
+# print(num ber)
+
+# num = 34 
+# print(type(num))
+# print(num)
+
+# num = float(num)
+# print(num)
+# print(type(num))
+
+# num = 4.5 
+# print(type(num))
+# print(num)
+
+# num = int(num)
+# print(type(num))
+# print(num)
+
+# num = '34'
+# print(type(num))
+# print(num)
+
+# num = int(num)
+# print(type(num))
+# print(num)
+
+# num = 34
+# print(type(num))
+# print(num)
+
+# num = str(num)
+# print(type(num))
+# print(num)
+
+# we cannot convert a string containing alphabets into an integer 
+# we can convert a string containing only numbers into an integer 
+
+# name = "Megan"
+# print(type(name))
+# print(name)
+
+# name = int(name)
+# print(type(name))
+# print(name)
+
+# case-sensitivity = lowecase and uppercase are different names 
+
+# name = "Megan"
+
+# Name = "Megan"
+
+# NAME = "Megan"
+
+# nAme = "Megan"
+
+# a = 1 
+# b = 2 
+# c = 4 
+
+# we can assign values to multiple variables in a single line like this: 
+# a, b, c = 1, 2, 4 
+
+# print(a)
+# print(b)
+# print(c)
+
+# a = b = c = 1 
+# print(a)
+# print(b)
+# print(c)
+
+# we can print multiple variables in single line using commas(,)
+# a = 3 
+# b = 4 
+# c = 7
+
+# print(a, b, c)
+
+# a = 3 
+# b = 4 
+# c = 7
+
+# print(a + b + c)
+
+
+# num = 34 
+# name = "Megan"
+
+# it will give error
+# we cannot add a string with a number 
+# print(num + name)
+
+# print(name + num)
+
+# strings 
+
+# mystr = "Megan"
+# print(mystr[2])
+
+# print(mystr[1])
+
+# print(mystr[0])
+
+# print(mystr[3])
